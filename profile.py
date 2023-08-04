949ca55bc143fec5d82a32a923eae9f4337eece8
wtj <zhangwt1997@gmail.com>
2023-08-04 22:09:23 +0800

    tailored for c8220 which has less RAM

--- profile.py
+++ profile.py
@@ -1,6 +1,6 @@
 """Profile for running Acto on CloudLab. There is one single physical node. The
 OS image is hardwired to Ubuntu 20.04. The hardware type is hardwired to
-`c6420`.
+`c8220`. **(This branch is mostly used when no `c6420` is available)**
 
 Instructions:
 Wait for the experiment to start, and then log into the node by either way:
@@ -32,7 +32,7 @@ request = pc.makeRequestRSpec()
 
 # Fixate parameters
 osImage  = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD'
-phystype = 'c6420'
+phystype = 'c8220'
 
 node = request.RawPC(hostname)
 node.disk_image    = osImage
