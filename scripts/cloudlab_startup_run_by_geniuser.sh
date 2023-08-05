d19a6e1910a239fd6512c1e88a57efb7d6fa5061
ZHANG Wentao <zhangwt1997@gmail.com>
2023-08-05 13:02:26 +0800

    redirect bash output

--- scripts/cloudlab_startup_run_by_geniuser.sh
+++ scripts/cloudlab_startup_run_by_geniuser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 experiment_creator=$( geni-get user_urn | rev | cut -d '+' -f -1 | rev )
-sudo su -c /local/repository/scripts/cloudlab_startup_run_by_creator.sh $experiment_creator
+sudo su -c /local/repository/scripts/cloudlab_startup_run_by_creator.sh $experiment_creator &> /tmp/startup_log
